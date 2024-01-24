5b68c8788ab1c3da8ccf9d96294898239cdaf5d5
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-24 15:45:37 +0000

    initialized regibox clone project using next js

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,11 @@
+export default function LandingPageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return(
+    <div className="h-full w-full flex items-center justify-center">
+      {children}
+    </div>
+  );
+};
