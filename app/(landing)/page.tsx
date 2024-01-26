dcd68b2b4d0cadeb24b242dc60ec74ec046121da
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-26 18:02:58 +0000

    created the home page with a call to action button

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,15 +3,35 @@ import Image from "next/image";
 export default function LandingPage() {
   return (
     <>
-      <div className="border w-full h-full bg-black/60 flex items-center justify-center">
+
+      <div className="w-full h-full bg-black/70 flex flex-col items-start justify-center relative">
+      <div className="absolute top-8 left-20">
+        <p className="font-bold text-neutral-50 text-3xl">
+          REGIBOX
+        </p>
+      </div>
         <Image
           src="/crossfit.jpeg"
           layout="fill"
           objectFit="cover"
           alt="Background image"
-          className="opacity-50 -z-50"
+          className="-z-50"
         />
-        <p className="text-white">olá eu sou o joão</p>
+        <div className="ml-20 mb-4">
+          <p className="drop-shadow-md text-3xl font-bold text-neutral-50">Revolutionize Your Fitness Journey</p>
+        </div>
+
+        <div className="w-[500px] ml-20 mb-4">
+          <p className="drop-shadow-md text-md text-neutral-50">
+            Seize control with our app, sculpting strength and resilience through seamlessly scheduled CrossFit sessions.
+            Transform your routine, empower your fitness – a digital revolution for a fitter, more vibrant you.</p>
+        </div>
+
+        <div className="ml-20 mb-3">
+          <button className="text-neutral-50 bg-green-500/75 py-2 px-4 rounded-md hover:bg-green-500/65">
+            Start your journey
+          </button>
+        </div>
       </div>
     </>
   );
