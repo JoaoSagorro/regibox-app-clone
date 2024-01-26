0dc5d4b6ccc4483a1f105ebc10e71221cb823edb
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-26 10:15:33 +0000

    added outfit font as the default font for the project

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 export default function LandingPage() {
   return (
     <>
-      <div className="border w-full h-full bg-black">
+      <div className="border w-full h-full bg-black/60 flex items-center justify-center">
         <Image
           src="/crossfit.jpeg"
           layout="fill"
           objectFit="cover"
           alt="Background image"
-          className="opacity-50"
+          className="opacity-50 -z-50"
         />
+        <p className="text-white">olá eu sou o joão</p>
       </div>
     </>
   );
