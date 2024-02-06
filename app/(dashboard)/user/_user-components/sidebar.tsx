4fefdde99552561b4ede76a4075717e03d25c988
Joao Sagorro <joao.sagorro@gmail.com>
2024-02-06 15:54:17 +0000

    styling navbar

--- app/(dashboard)/user/_user-components/sidebar.tsx
+++ app/(dashboard)/user/_user-components/sidebar.tsx
@@ -1,7 +1,7 @@
 const UserSidebar = () => {
   return(
     <div className="border-slate-200 border-r-2 rounded-r-md bg-[#cadfe5] h-screen w-16 duration-700 hover:rounded-none hover:w-96 hover:duration-500 ">
-      This is my sidebar!
+      <p className="invisible hover:visible hover:text-black">This is my sidebar!</p>
     </div>
   );
 };
