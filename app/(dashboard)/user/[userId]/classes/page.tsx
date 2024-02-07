86bad233eb3f166e0cc6e64396345161fe7549ee
Joao Sagorro <joao.sagorro@gmail.com>
2024-02-07 12:16:43 +0000

    trying to figure out how to use the dynamic routes do change pages with the sidebar icons

---
+++ app/(dashboard)/user/[userId]/classes/page.tsx
@@ -0,0 +1,7 @@
+const ClassesPage = () => {
+  return(
+    <div>
+      This is where I can see all my classes!
+    </div>
+  );
+};
--- app/(dashboard)/user/_user-components/sidebar.tsx
+++ app/(dashboard)/user/_user-components/sidebar.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCalendar,
   faDumbbell,
 } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const UserSidebar = () => {
+
+  const router = useRouter();
+
   return(
     <div className="border-slate-200 border-r-2 rounded-r-md bg-[#cadfe5] h-screen w-16 duration-700 hover:rounded-none hover:w-96 hover:duration-500 flex flex-col relative">
       <p className="hover:visible hover:text-black">This is my sidebar!</p>
-      <FontAwesomeIcon icon={faCalendar} className='text-white w-8 absolute left-3 top-20'/>
+      <Link href={`${router.push("/classes")}`}>
+        <FontAwesomeIcon icon={faCalendar} className='text-white w-8 absolute left-3 top-20'/>
+      </Link>
       <FontAwesomeIcon icon={faDumbbell} className='text-white w-8 absolute left-3 top-36'/>
     </div>
   );
