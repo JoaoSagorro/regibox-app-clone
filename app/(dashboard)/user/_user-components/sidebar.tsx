7646399a91c09f4355c81387fe48eec53bceaf19
Joao Sagorro <joao.sagorro@gmail.com>
2024-02-07 12:23:34 +0000

    need to fix the connection between the form and database

--- app/(dashboard)/user/_user-components/sidebar.tsx
+++ app/(dashboard)/user/_user-components/sidebar.tsx
@@ -8,16 +8,25 @@ import {
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-const UserSidebar = () => {
+interface Users {
+  id: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+}
+
+const UserSidebar = ({ id, firstName, lastName, email }: Users) => {
 
   const router = useRouter();
 
   return(
     <div className="border-slate-200 border-r-2 rounded-r-md bg-[#cadfe5] h-screen w-16 duration-700 hover:rounded-none hover:w-96 hover:duration-500 flex flex-col relative">
       <p className="hover:visible hover:text-black">This is my sidebar!</p>
-      <Link href={`${router.push("/classes")}`}>
-        <FontAwesomeIcon icon={faCalendar} className='text-white w-8 absolute left-3 top-20'/>
-      </Link>
+      {Users.map((user) => (
+        <Link key={user.id} href={`/user/${encodeURIComponent(user.userId)}/classes`}>
+          <FontAwesomeIcon icon={faCalendar} className='text-white w-8 absolute left-3 top-20'/>
+        </Link>
+      ))}
       <FontAwesomeIcon icon={faDumbbell} className='text-white w-8 absolute left-3 top-36'/>
     </div>
   );
