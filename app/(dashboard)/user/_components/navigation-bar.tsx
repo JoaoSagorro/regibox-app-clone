fe66f5ea5cbbee2a2e3ca6556c310b2c2afc1ffc
Joao Sagorro <joao.sagorro@gmail.com>
2024-02-28 11:05:57 +0000

    navbar almost done, links are missing. Going to start working on the schedule page first

--- app/(dashboard)/user/_components/navigation-bar.tsx
+++ app/(dashboard)/user/_components/navigation-bar.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/navigation-menu";
 
 interface NavigationBarProps {
+  children?: React.ReactNode;
   title: string;
   className?: string;
   link: string;
@@ -19,6 +20,7 @@ interface NavigationBarProps {
 // TODO: each component must have a title and a link for the specific page
 
 export const NavigationBar = ({
+  children,
   title,
   className,
   link,
@@ -28,10 +30,10 @@ export const NavigationBar = ({
       <NavigationMenuList className="w-full">
 
           <NavigationMenuItem className="font-semibold">
-            <NavigationMenuTrigger> {title} </NavigationMenuTrigger>
+            <NavigationMenuTrigger className="text-md">{title}</NavigationMenuTrigger>
             <NavigationMenuContent className="w-24 flex items-center justify-start ml-2">
               <div className="w-[200px]">
-                <NavigationMenuLink href={link}>This is a link</NavigationMenuLink>
+                <NavigationMenuLink href={link} className="text-sm">{children}</NavigationMenuLink>
               </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
--- app/(dashboard)/user/layout.tsx
+++ app/(dashboard)/user/layout.tsx
@@ -10,20 +10,40 @@ const UserLayout = ({
   return(
     <>
       <div className="w-full sticky top-0 flex justify-evenly border-black border-2 bg-white py-3">
-        <NavigationBar title="Classes" link='/users/1/classes'/>
-        <NavigationBar title="Workouts" link='/users/1/classes' />
-        <NavigationBar title="Records" link='/users/1/classes' />
-        <NavigationBar title="Activities" link='/users/1/classes' />
-        <NavigationBar title="Store" link='/users/1/classes' />
-        <NavigationBar title="Challenges" link='/users/1/classes' />
-        <NavigationBar title="Box Ranking" link='/users/1/classes' />
-        <NavigationBar title="Comments" link='/users/1/classes' />
-        <NavigationBar title="My Box" link='/users/1/classes' />
-        <NavigationBar title="Convertors" link='/users/1/classes' />
-        <NavigationBar title="Timers" link='/users/1/classes' />
-        <NavigationBar title="Settings" link='/users/1/classes' />
-        <NavigationBar title="Terms of Use" link='/users/1/classes' />
-        <NavigationBar title="Logout" link='/login' />
+        <NavigationBar title="Classes" link='/users/1/classes'>
+          <ul>
+            <li className="my-2">Schedule</li>
+            <li className="my-2">Check-in</li>
+            <li className="my-2">Services</li>
+            <li className="my-2">Drop-in</li>
+            <li className="my-2">History</li>
+          </ul>
+        </NavigationBar>
+        <NavigationBar title="Workouts" link='/users/1/classes'>
+          <ul>
+            <li className="my-2">Box workouts</li>
+            <li className="my-2">Individual</li>
+            <li className="my-2">Movements</li>
+            <li className="my-2">History</li>
+          </ul>
+        </NavigationBar>
+        <NavigationBar title="Records" link='/users/1/classes'>
+          <ul>
+            <li className="my-2">Personal</li>
+            <li className="my-2">Box</li>
+          </ul>
+        </NavigationBar>
+        <NavigationBar title="Activities" link='/users/1/classes'/>
+        <NavigationBar title="Store" link='/users/1/classes'/>
+        <NavigationBar title="Challenges" link='/users/1/classes'/>
+        <NavigationBar title="Box Ranking" link='/users/1/classes'/>
+        <NavigationBar title="Comments" link='/users/1/classes'/>
+        <NavigationBar title="My Box" link='/users/1/classes'/>
+        <NavigationBar title="Convertors" link='/users/1/classes'/>
+        <NavigationBar title="Timers" link='/users/1/classes'/>
+        <NavigationBar title="Settings" link='/users/1/classes'/>
+        <NavigationBar title="Terms of Use" link='/users/1/classes'/>
+        <NavigationBar title="Logout" link='/login'/>
       </div>
 
       <div className="bg-[#cadfe5] flex w-full h-screen">
